02f5dfe50ee6815e524d5e8eea72859e3871078f
Rubi Ben-Zvi <rubi.benzvi@gmail.com>
2023-02-18 19:25:58 +0200

    after change name

--- app/src/main/java/com/example/Dbtime_Mobile/MainActivity7.java
+++ app/src/main/java/com/example/Dbtime_Mobile/MainActivity7.java
@@ -27,26 +27,29 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 
 public class MainActivity7 extends AppCompatActivity {
 
     public Long dtNmbr= Long.valueOf(new Date().getTime());
-    //public Long dtNmbr, dtNmbrInDB;
     public Integer i = 0;
     public Integer second = 1000;
     public Integer minute = 60*second;
     public Integer hour = 60*minute;
     public Integer day = 24*hour;
-    public ArrayList target,from;
+    public ArrayList target,from,targetArr,newTargetArr;
     public String txt;
     public long dif, nowMilli;
     public DocumentReference userTargets;
+    TextView tv,tv18,tv24,tv26,tv28,tv30;
+    ProgressBar pb3,pb4,pb5,pb6;
 
     private FirebaseAuth mAuth;
     long time= System.currentTimeMillis();
@@ -59,6 +62,19 @@ public class MainActivity7 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main7);
 
+        tv = (TextView)findViewById(R.id.textView15);
+        TextInputEditText inpt = (TextInputEditText) findViewById(R.id.inpt71);
+        Button entr = (Button) findViewById(R.id.button5);
+        tv18 = (TextView)findViewById(R.id.tv18);
+        tv24 = (TextView)findViewById(R.id.textView24);
+        tv26 = (TextView)findViewById(R.id.textView26);
+        tv28 = (TextView)findViewById(R.id.textView28);
+        tv30 = (TextView)findViewById(R.id.textView30);
+        pb3 = (ProgressBar) findViewById(R.id.pb3);
+        pb4 = (ProgressBar) findViewById(R.id.pb4);
+        pb5 = (ProgressBar) findViewById(R.id.pb5);
+        pb6 = (ProgressBar) findViewById(R.id.pb6);
+
         TextView link = (TextView) findViewById(R.id.hiperlink7);
         link.setMovementMethod(LinkMovementMethod.getInstance());
 
@@ -67,41 +83,7 @@ public class MainActivity7 extends AppCompatActivity {
         email = user.getEmail();
         final Calendar c = Calendar.getInstance();
 
-        TextInputEditText inpt = (TextInputEditText) findViewById(R.id.inpt71);
-        Button entr = (Button) findViewById(R.id.button5);
-        TextView tv = (TextView)findViewById(R.id.textView15);
-        TextView tv18 = (TextView)findViewById(R.id.tv18);
-        TextView tv24 = (TextView)findViewById(R.id.textView24);
-        TextView tv26 = (TextView)findViewById(R.id.textView26);
-        TextView tv28 = (TextView)findViewById(R.id.textView28);
-        TextView tv30 = (TextView)findViewById(R.id.textView30);
-        ProgressBar pb3 = (ProgressBar) findViewById(R.id.pb3);
-        ProgressBar pb4 = (ProgressBar) findViewById(R.id.pb4);
-        ProgressBar pb5 = (ProgressBar) findViewById(R.id.pb5);
-        ProgressBar pb6 = (ProgressBar) findViewById(R.id.pb6);
-
-        userTargets = db.collection("users").document(email)
-                .collection("targets").document("list");
-        userTargets.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if (task.isSuccessful()) {
-                    DocumentSnapshot document = task.getResult();
-                    if (document.exists()) {
-                        from = new ArrayList(document.getData().keySet());
-                        target = new ArrayList(document.getData().values());
-                        txt = target.get(0).toString();
-                        tv.setText(txt);
-                        dtNmbr = valueOf((String) from.get(0));
-
-                    } else {
-                        Log.d("Rubi", "No such document");
-                    }
-                } else {
-                    Log.d("Rubi", "get failed with ", task.getException());
-                }
-            }
-        });
+        getTargetsFromBackEnd();
 
         pb3.setMax(365);
         pb4.setMax(24);
@@ -114,46 +96,55 @@ public class MainActivity7 extends AppCompatActivity {
             public long time = 0;
 
             public void run() {
-                nowMilli = new Date().getTime();
-                dif = nowMilli-dtNmbr;
-                Integer difdays = Math.toIntExact(dif / day);
-                Integer difhors = Math.toIntExact((dif- (long) difdays *day )/ hour);
-                Integer difminuts = Math.toIntExact((dif- (long) difdays *day- (long) difhors *hour)/ minute);
-                Integer difseconds = Math.toIntExact((dif- (long) difdays *day- (long) difhors *hour- (long) difminuts *minute)/ second);
+                if (target!= null){
+                    nowMilli = new Date().getTime();
+                    dif = nowMilli - dtNmbr;
+                    Integer difdays = Math.toIntExact(dif / day);
+                    Integer difhors = Math.toIntExact((dif - (long) difdays * day) / hour);
+                    Integer difminuts = Math.toIntExact((dif - (long) difdays * day - (long) difhors * hour) / minute);
+                    Integer difseconds = Math.toIntExact((dif - (long) difdays * day - (long) difhors * hour - (long) difminuts * minute) / second);
 
-                tv24.setText(String.valueOf(difdays));
-                tv26.setText(String.valueOf(difhors));
-                tv28.setText(String.valueOf(difminuts));
-                tv30.setText(String.valueOf(difseconds));
-                pb3.setProgress(difdays);
-                pb4.setProgress(difhors);
-                pb5.setProgress(difminuts);
-                pb6.setProgress(difseconds);
-                tv18.setText(getDate(dtNmbr,c));
-                time += 1000;
-                h.postDelayed(this, 1000);
+                    tv24.setText(String.valueOf(difdays));
+                    tv26.setText(String.valueOf(difhors));
+                    tv28.setText(String.valueOf(difminuts));
+                    tv30.setText(String.valueOf(difseconds));
+                    pb3.setProgress(difdays);
+                    pb4.setProgress(difhors);
+                    pb5.setProgress(difminuts);
+                    pb6.setProgress(difseconds);
+                    tv18.setText(getDate(dtNmbr, c));
+                    time += 1000;
+                    h.postDelayed(this, 1000);
+                }
             }
         }, 1000); // 1 second delay (takes millis)
 
         findViewById(R.id.frwrd).setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                if(i<target.size()-1) i++;
-                else i = 0;
-                txt = target.get(i).toString();
-                tv.setText(txt);
-                dtNmbr = valueOf((String) from.get(i));
-                tv18.setText(getDate(dtNmbr,c));
+                if(target!=null) {
+                    if (i < target.size() - 1) i++;
+                    else i = 0;
+                    targetArr = (ArrayList) target.get(i);
+                    txt = targetArr.get(0).toString();
+                    //txt = (target.get(i)).get(0).toString();
+                    tv.setText(txt);
+                    dtNmbr = valueOf((String) from.get(i));
+                    tv18.setText(getDate(dtNmbr, c));
+                }
             }
         });
 
         findViewById(R.id.bckwrd).setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                if(i>0) i--;
-                else i = target.size()-1;
-                txt = target.get(i).toString();
-                tv.setText(txt);
-                dtNmbr = valueOf((String) from.get(i));
-                tv18.setText(getDate(dtNmbr,c));
+                if (target != null) {
+                    if (i > 0) i--;
+                    else i = target.size() - 1;
+                    targetArr = (ArrayList) target.get(i);
+                    txt = targetArr.get(0).toString();
+                    tv.setText(txt);
+                    dtNmbr = valueOf((String) from.get(i));
+                    tv18.setText(getDate(dtNmbr, c));
+                }
             }
         });
 
@@ -172,7 +163,6 @@ public class MainActivity7 extends AppCompatActivity {
             public void onClick(View view) {
                 fltBt.setVisibility((View.VISIBLE));
                 String t =inpt.getText().toString();
-                target.add(t);
                 Date dd = new Date();
                 Long ddms = dd.getTime();
                 from.add(String.valueOf(ddms));
@@ -189,7 +179,6 @@ public class MainActivity7 extends AppCompatActivity {
         // Reset start date
         findViewById(R.id.button7).setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-               // Log.d("Rubi","#1: "+dtNmbr.toString());
                 updateStartTarget(dtNmbr.toString(),txt);
                 Date dd = new Date();
                 Long ddms = dd.getTime();
@@ -208,9 +197,13 @@ public class MainActivity7 extends AppCompatActivity {
         return (day + "-" + (month + 1) + "-" + year + " ");
     }
 
-    public void addTarget(String target){
+    public void addTarget(String theNewTarget){
         Map<String, Object> newTarget = new HashMap<>();
-        newTarget.put(timeMS, target);
+        newTargetArr = new ArrayList();
+        newTargetArr.add(theNewTarget);
+        newTargetArr.add(604800000);
+        newTargetArr.add("שבוע");
+        newTarget.put(timeMS, newTargetArr);
 
         db.collection("users").document(email)
         .collection("targets").document("list")
@@ -219,6 +212,9 @@ public class MainActivity7 extends AppCompatActivity {
             @Override
             public void onSuccess(Void aVoid) {
                 Log.d("Rubi", "DocumentSnapshot successfully written!");
+                i++;
+                from.add(timeMS);
+                target.add(newTargetArr);
             }
         })
         .addOnFailureListener(new OnFailureListener() {
@@ -230,32 +226,12 @@ public class MainActivity7 extends AppCompatActivity {
     }
 
     public void updateStartTarget(String newStartMS,String trgt){
-        Map<String, Object> newTarget = new HashMap<>();
-        nowMilli = new Date().getTime();
         dif = nowMilli-dtNmbr;
-
+        addTarget(trgt); // Add updated target
+        // Delete the old target
         Map<String,Object> toDelete = new HashMap<>();
         toDelete.put(dtNmbr.toString(), FieldValue.delete());
-        newTarget.put(String.valueOf(nowMilli), trgt);
 
-        // Add the new/update target
-        db.collection("users").document(email)
-        .collection("targets").document("list")
-        .update(newTarget)
-        .addOnSuccessListener(new OnSuccessListener<Void>() {
-            @Override
-            public void onSuccess(Void aVoid) {
-                Log.d("Rubi", "DocumentSnapshot successfully written!");
-            }
-        })
-        .addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Log.d("Rubi", "Error writing document", e);
-            }
-        } );
-
-        // Delete the old target
         db.collection("users").document(email)
         .collection("targets").document("list")
         .update(toDelete)
@@ -271,5 +247,31 @@ public class MainActivity7 extends AppCompatActivity {
                 Log.d("Rubi", "Error writing document", e);
             }
         } );
+    }
+
+    public void getTargetsFromBackEnd(){
+        userTargets = db.collection("users").document(email)
+                .collection("targets").document("list");
+        userTargets.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()&&!document.getData().isEmpty()) {
+                        from = new ArrayList(document.getData().keySet());
+                        target = new ArrayList(document.getData().values());
+                        targetArr = (ArrayList) target.get(0);
+                        txt = targetArr.get(0).toString();
+                        tv.setText(txt);
+                        dtNmbr = valueOf((String) from.get(0));
+
+                    } else {
+                        Log.d("Rubi", "No such document");
+                    }
+                } else {
+                    Log.d("Rubi", "get failed with ", task.getException());
+                }
+            }
+        });
     }
 }
