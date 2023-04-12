410a6d2f9df64f8f1628793400fe956e41446e26
Lion Burger <lionbur@gmail.com>
2023-04-12 12:36:01 +0300

    merge

--- app/src/main/java/com/example/Dbtime_Mobile/MindfulCard.java
+++ app/src/main/java/com/example/Dbtime_Mobile/MindfulCard.java
@@ -15,45 +15,40 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class MindfulCard extends Fragment {
+    private static final String ARG_CATEGORY = "category";
+    String category;
+    View frontface, backface;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public MindfulCard() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MindfulCard.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MindfulCard newInstance(String param1, String param2) {
+    public static MindfulCard newInstance(String category) {
         MindfulCard fragment = new MindfulCard();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString(ARG_CATEGORY, category);
         fragment.setArguments(args);
         return fragment;
     }
 
+    ValueAnimator flipAnimator;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            this.category = getArguments().getString(ARG_CATEGORY);
         }
+
+        View view = getView();
+
+        frontface = view.findViewById(R.id.mindful_card_frontface);
+        backface = view.findViewById(R.id.mindful_card_backface);
+
+        flipAnimator = ValueAnimator.ofFloat(0.5f, 0f);
+        flipAnimator.addUpdateListener(new FlipListener(this.frontface, this.backface));
+        flipAnimator.setDuration(2000);
+        flipAnimator.start();
     }
 
     @Override
@@ -64,7 +59,7 @@ public class MindfulCard extends Fragment {
     }
 }
 
-public class FlipListener implements ValueAnimator.AnimatorUpdateListener {
+class FlipListener implements ValueAnimator.AnimatorUpdateListener {
 
     private final View mFrontView;
     private final View mBackView;
